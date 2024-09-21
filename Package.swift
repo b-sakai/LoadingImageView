dd764146fd9c7c524b779f2862008e57544f7b21
b-sakai <blueberryoctopas@gmail.com>
2024-09-21 13:58:57 +0900

    first commit

--- Package.swift
+++ Package.swift
@@ -5,17 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "LoadingImageView",
+    platforms: [
+        .iOS(.v17)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "LoadingImageView",
-            targets: ["LoadingImageView"]),
+            targets: ["LoadingImageView"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "LoadingImageView"),
+            name: "LoadingImageView",
+            dependencies: [],
+            resources: [.process("Shaders/Ripple.metal")]
+        ),
         .testTarget(
             name: "LoadingImageViewTests",
             dependencies: ["LoadingImageView"]),
